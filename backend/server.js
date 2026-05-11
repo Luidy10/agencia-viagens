bf929b3fee7f01a522887ec6cfdbfb346ba68616
Luidy <luidyluiz76@gmail.com>
2026-05-11 19:28:43 -0300

    corrige porta smtp para render

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,9 @@ const Viagem     = require("./models/Viagem")
 const Comentario = require("./models/Comentario")
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS

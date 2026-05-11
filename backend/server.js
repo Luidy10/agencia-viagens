442e7a92269a1b975061d71db349ce120f24e8b6
Luidy <luidyluiz76@gmail.com>
2026-05-11 20:01:21 -0300

    corrige cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const axios = require("axios")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://agencia-viagens-ashen.vercel.app",
+  credentials: true
+}))
 app.use(express.json())
 
 app.use("/uploads", express.static("uploads"))
